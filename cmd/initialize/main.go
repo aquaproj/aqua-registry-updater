28bd0fc38b5a88f2557d9b838f54c2c539745749
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2023-05-22 20:23:08 +0900

    feat: add initialize command

---
+++ cmd/initialize/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/afero"
+	"github.com/suzuki-shunsuke/aqua-registry-updater/pkg/controller/initcmd"
+	"github.com/suzuki-shunsuke/aqua-registry-updater/pkg/log"
+	"github.com/suzuki-shunsuke/logrus-error/logerr"
+)
+
+var version = ""
+
+func main() {
+	logE := log.New(version)
+	if err := core(context.Background(), logE); err != nil {
+		logerr.WithError(logE, err).Fatal("aqua-registry-updater failed")
+	}
+}
+
+func core(ctx context.Context, logE *logrus.Entry) error {
+	token := os.Getenv("GITHUB_TOKEN")
+	ctrl := initcmd.New(afero.NewOsFs())
+	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
+	defer stop()
+	return ctrl.Init(ctx, logE, &initcmd.Param{
+		GitHubToken: token,
+	}) //nolint:wrapcheck
+}
