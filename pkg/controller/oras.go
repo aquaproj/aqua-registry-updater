5692b2cf497ce6fa0a5ec800936a74f3e09c54e4
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2023-11-04 13:24:07 +0900

    refactor: split files

---
+++ pkg/controller/oras.go
@@ -0,0 +1,89 @@
+package controller
+
+import (
+	"context"
+	"fmt"
+
+	v1 "github.com/opencontainers/image-spec/specs-go/v1"
+	"oras.land/oras-go/v2"
+	"oras.land/oras-go/v2/content/file"
+	"oras.land/oras-go/v2/registry/remote"
+	"oras.land/oras-go/v2/registry/remote/auth"
+	"oras.land/oras-go/v2/registry/remote/retry"
+)
+
+type ContainerRegistry struct {
+	Registry   string
+	Repository string
+	Auth       *ContainerRegistryAuth
+}
+
+type ContainerRegistryAuth struct {
+	Username string
+}
+
+func (c *Controller) newRepo(reg *ContainerRegistry, token string) (*remote.Repository, error) {
+	repo, err := remote.NewRepository(reg.Registry + "/" + reg.Repository)
+	if err != nil {
+		return nil, fmt.Errorf("create a client for a remote repository: %w", err)
+	}
+	repo.Client = &auth.Client{
+		Client: retry.DefaultClient,
+		Cache:  auth.DefaultCache,
+		Credential: auth.StaticCredential(reg.Registry, auth.Credential{
+			Username: reg.Auth.Username,
+			Password: token,
+		}),
+	}
+	return repo, nil
+}
+
+func pushFiles(ctx context.Context, repo *remote.Repository, tag string) error {
+	fs, err := file.New("")
+	if err != nil {
+		return fmt.Errorf("create a file store: %w", err)
+	}
+	defer fs.Close()
+	mediaType := "example/file" // "application/vnd.unknown.config.v1+json"
+	fileNames := []string{"data.json"}
+	fileDescriptors := make([]v1.Descriptor, 0, len(fileNames))
+	for _, name := range fileNames {
+		fileDescriptor, err := fs.Add(ctx, name, mediaType, "")
+		if err != nil {
+			return fmt.Errorf("add a file to the file store: %w", err)
+		}
+		fileDescriptors = append(fileDescriptors, fileDescriptor)
+	}
+
+	// 2. Pack the files and tag the packed manifest
+	artifactType := "example/files"
+	manifestDescriptor, err := oras.Pack(ctx, fs, artifactType, fileDescriptors, oras.PackOptions{
+		PackImageManifest: true,
+	})
+	if err != nil {
+		return fmt.Errorf("pack files: %w", err)
+	}
+
+	if err := fs.Tag(ctx, manifestDescriptor, tag); err != nil {
+		return fmt.Errorf("tag the packed manifest: %w", err)
+	}
+
+	if _, err := oras.Copy(ctx, fs, tag, repo, tag, oras.DefaultCopyOptions); err != nil {
+		return fmt.Errorf("copy from the file store to the remote repository: %w", err)
+	}
+	return nil
+}
+
+func pullFiles(ctx context.Context, repo *remote.Repository, tag string) error {
+	// 0. Create a file store
+	fs, err := file.New("")
+	if err != nil {
+		return fmt.Errorf("create a file store: %w", err)
+	}
+	defer fs.Close()
+
+	if _, err := oras.Copy(ctx, repo, tag, fs, tag, oras.DefaultCopyOptions); err != nil {
+		return fmt.Errorf("copy from the remote repository to the file store: %w", err)
+	}
+	return nil
+}
